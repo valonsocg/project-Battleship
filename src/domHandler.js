1d28570d471f3c76872a68b48eeaf9512286a9bb
alonso-dev <valonsocg@gmail.com>
2024-11-23 11:02:44 -0500

    fix logic in game and domHandler , add ui elements to html to better reflect 2 boards per player

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,7 +1,8 @@
 const DOMHandler = (() => {
-  function renderGameboard(gameboard, playerType) {
-    const boardContainer = document.querySelector(`#${playerType}-board`);
-
+  function renderGameboard(gameboard, boardType, playerType) {
+    const boardContainer = document.querySelector(
+      `#${playerType}-${boardType}`
+    );
     boardContainer.innerHTML = "";
 
     gameboard.grid.forEach((row, rowIndex) => {
@@ -9,12 +10,16 @@ const DOMHandler = (() => {
         const cellDiv = document.createElement("div");
         cellDiv.classList.add("cell");
 
-        if (cell === "ship" && playerType === "player") {
-          cellDiv.classList.add("ship");
-        } else if (cell === "hit") {
-          cellDiv.classList.add("hit");
-        } else if (cell === "miss") {
-          cellDiv.classList.add("miss");
+        if (boardType === "board") {
+          if (playerType.startsWith("player") && cell === "ship") {
+            cellDiv.classList.add("ship");
+          }
+        } else if (boardType === "attacks") {
+          if (cell === "hit") {
+            cellDiv.classList.add("hit");
+          } else if (cell === "miss") {
+            cellDiv.classList.add("miss");
+          }
         }
 
         cellDiv.dataset.coord = `${rowIndex},${colIndex}`;
@@ -23,10 +28,11 @@ const DOMHandler = (() => {
     });
   }
 
-  function setupAttackListener(callback) {
-    const opponentBoard = document.querySelector("#opponent-board");
+  function setupAttackListener(player1Callback, player2Callback) {
+    const player1Board = document.querySelector("#player1-attacks");
+    const player2Board = document.querySelector("#player2-attacks");
 
-    opponentBoard.addEventListener("click", (e) => {
+    player1Board.addEventListener("click", (e) => {
       const target = e.target;
 
       if (
@@ -38,15 +44,34 @@ const DOMHandler = (() => {
           const [row, col] = target.dataset.coord.split(",").map(Number);
 
           if (!isNaN(row) && !isNaN(col)) {
-            callback([row, col]);
+            player1Callback([row, col]);
+          }
+        }
+      }
+    });
+
+    player2Board.addEventListener("click", (e) => {
+      const target = e.target;
+
+      if (
+        target.classList.contains("cell") &&
+        !target.classList.contains("hit") &&
+        !target.classList.contains("miss")
+      ) {
+        if (target.dataset.coord) {
+          const [row, col] = target.dataset.coord.split(",").map(Number);
+
+          if (!isNaN(row) && !isNaN(col)) {
+            player2Callback([row, col]);
           }
         }
       }
     });
   }
 
-  function updateGameboard(gameboard, playerType) {
-    renderGameboard(gameboard, playerType);
+  function updateGameboards(playerGameboard, opponentGameboard, playerType) {
+    renderGameboard(playerGameboard, "board", playerType);
+    renderGameboard(opponentGameboard, "attacks", playerType);
   }
 
   function displayWinner(winner) {
@@ -58,7 +83,7 @@ const DOMHandler = (() => {
   return {
     renderGameboard,
     setupAttackListener,
-    updateGameboard,
+    updateGameboards,
     displayWinner,
   };
 })();
--- src/game.js
+++ src/game.js
@@ -1,41 +1,49 @@
-import DOMHandler from "./domHandler";
 import Player from "./player";
 import Ship from "./ship";
+import DOMHandler from "./domHandler";
 
 const Game = (() => {
-  const player = new Player("real");
-  const computer = new Player("computer");
+  const player1 = new Player("real");
+  const player2 = new Player("computer");
 
   function setupBoards() {
-    player.gameboard.placeShip(new Ship(4), [0, 0], "horizontal");
-    player.gameboard.placeShip(new Ship(3), [3, 3], "vertical");
-    player.gameboard.placeShip(new Ship(2), [7, 5], "horizontal");
+    player1.gameboard.placeShip(new Ship(4), [0, 0], "horizontal");
+    player1.gameboard.placeShip(new Ship(3), [3, 3], "vertical");
+    player1.gameboard.placeShip(new Ship(2), [7, 5], "horizontal");
 
-    computer.gameboard.placeShip(new Ship(4), [1, 1], "vertical");
-    computer.gameboard.placeShip(new Ship(3), [5, 0], "horizontal");
-    computer.gameboard.placeShip(new Ship(2), [8, 6], "vertical");
+    player2.gameboard.placeShip(new Ship(4), [1, 1], "vertical");
+    player2.gameboard.placeShip(new Ship(3), [5, 0], "horizontal");
+    player2.gameboard.placeShip(new Ship(2), [8, 6], "vertical");
   }
 
-  let currentPlayer = player;
+  let currentPlayer = player1;
 
   function switchTurn() {
-    currentPlayer = currentPlayer === player ? computer : player;
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
   }
 
   function handleAttack(coord) {
-    const opponent = currentPlayer === player ? computer : player;
+    const opponent = currentPlayer === player1 ? player2 : player1;
     const result = opponent.gameboard.receiveAttack(coord);
 
-    DOMHandler.updateGameboard(opponent.gameboard, "opponent");
-    DOMHandler.updateGameboard(currentPlayer.gameboard, "player");
+    DOMHandler.updateGameboards(
+      player1.gameboard,
+      player2.gameboard,
+      "player1"
+    );
+    DOMHandler.updateGameboards(
+      player2.gameboard,
+      player1.gameboard,
+      "player2"
+    );
 
     if (opponent.gameboard.allShipsSunked()) {
       DOMHandler.displayWinner(
-        currentPlayer === player ? "player" : "computer"
+        currentPlayer === player1 ? "Player 1" : "Player 2"
       );
     } else {
       switchTurn();
-      if (currentPlayer === computer) {
+      if (currentPlayer === player2) {
         setTimeout(() => {
           const randomCoord = currentPlayer.getRandomCoord(
             opponent.gameboard.size,
@@ -50,12 +58,19 @@ const Game = (() => {
   function startGame() {
     setupBoards();
 
-    const opponent = computer;
-    DOMHandler.renderGameboard(player.gameboard, "player");
-    DOMHandler.renderGameboard(opponent.gameboard, "opponent");
+    DOMHandler.updateGameboards(
+      player1.gameboard,
+      player2.gameboard,
+      "player1"
+    );
+    DOMHandler.updateGameboards(
+      player2.gameboard,
+      player1.gameboard,
+      "player2"
+    );
 
     DOMHandler.setupAttackListener((coord) => {
-      if (currentPlayer === player) {
+      if (currentPlayer === player1) {
         handleAttack(coord);
       }
     });
